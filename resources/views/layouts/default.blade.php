31525e5715d2781c70b6434fbfcfc6f55942ae21
PSBLaLaHey <pizz12547@gmail.com>
2024-02-28 08:45:00 +0700

    test

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -19,6 +19,8 @@
             <div class="text-[1.6rem]">
                 <h1>LOGO</h1>
             </div>
+
+            
             <div class="w-full flex justify-end ">
                 <img class="w-[2.5rem] rounded-full border border-[#5d87ff]"
                     src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
