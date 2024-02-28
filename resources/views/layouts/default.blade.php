9a8a8b82725fc04a6554561a015125a87f2af930
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-02-28 21:47:46 +0700

    refactor: tailwind css import

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('../../resources/css/app.css')
+    @vite('resources/css/app.css')
     <title>WMS | @yield('title',"WMS + Iot")</title>
 </head>
 
