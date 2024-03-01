5adee5de01d1837458ca50305d6f8c053f0e24fd
Chaimanat2546 <152174623+Chaimanat2546@users.noreply.github.com>
2024-03-01 17:02:16 +0700

    test test 123

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/dashboard/analytic', function () {
     return view('dashboards.v_all_wh');
 });
-
+//ไม่เอาไม่ทำไม่ชอบ ผมอยากนอนละพี่ อุแว้ๆๆๆๆๆๆๆๆๆๆ
 Route::get('/dashboard/view-another', function () {
     return view('dashboards.v_single_wh');
 });
