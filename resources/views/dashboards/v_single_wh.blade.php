04ee1f8fb5ee52f36b2442e15a336595377bf699
Amonpan2 <65160241@go.buu.ac.th>
2024-03-02 00:17:14 +0700

    Test_Dashboard

--- resources/views/dashboards/v_single_wh.blade.php
+++ resources/views/dashboards/v_single_wh.blade.php
@@ -3,7 +3,13 @@
 @section('title', 'ดูคลังสินค้าอื่น')
 @section('content')
 <div style="height: calc(100vh - 4rem)" class="bg-[#F6F9FC] border w-full flex flex-col overflow-y-scroll">
-    <div class="mt-[5rem] md:mt-0">
+    <div class="mt-[5rem] md:mt-0 mx-3">
+        <p class="font-bold text-sm mt-3 ">แดชบอร์ด/ดูคลังสินค้าอื่น</p>
+        <div class="mt-5">
+            <p class="font-bold text-sm mx-6">ตารางแสดงรายการ Warehouse</p>
+            
+        </div>
+
         <h1>ดูแยก WH1</h1>
         <h1>ดูแยก WH2</h1>
         <h1>ดูแยก WH3</h1>
