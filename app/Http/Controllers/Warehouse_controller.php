49a664b97b5b1a0f8e899e5544c56919f5ee1d21
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-03-11 14:56:56 +0700

    feat: implement role and permission

--- app/Http/Controllers/Warehouse_controller.php
+++ app/Http/Controllers/Warehouse_controller.php
@@ -4,20 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Warehouse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
 class Warehouse_controller extends Controller
 {
 
-    public function index_warehouse(){
+    public function index_warehouse()
+    {
         try {
 
             $warehouse_id = Session::get('user_warehouse');
-            if($warehouse_id == null){
-                $warehouse_id = 1;
+            if ($warehouse_id == null) {
+                $warehouse_id = Auth::user()->warehouses[0]->wh_id;
             }
 
-            //dd($warehouse_id);
 
             $wh = Warehouse::where('wh_id', $warehouse_id)->first();
 
@@ -25,7 +26,6 @@ class Warehouse_controller extends Controller
                 return back()->withErrors(['get_warehouse' => "ไม่มี"]);
             }
 
-            //dd($wh->wh_name);
             return view('dashboards.v_all_wh')->with('wh', $wh);
 
         } catch (\Exception $e) {
@@ -37,33 +37,15 @@ class Warehouse_controller extends Controller
     {
         try {
             $warehouse_id = $request->only('warehouse_id');
-            $warehouse = Warehouse::where('wh_id', $warehouse_id);
             Session::put('user_warehouse', $warehouse_id);
 
-            $user_wh = Session::get('user_warehouse');
-            return response()->json(['success' => true, 'user_wh' => $user_wh], 200);
+            $warehouses = Warehouse::where('wh_id', $warehouse_id)->first();
+            Session::put('user_warehouse_name', $warehouses->wh_name);
+
+            return response()->json(['success' => true], 200);
 
         } catch (\Exception $e) {
             return back()->withErrors(['change_warehouse' => $e->getMessage()]);
-        }
-    }
-
-    public function get_warehouse(Request $request)
-    {
-        try {
-
-            $warehouse_id = Session::get('user_warehouse');
-
-            $wh = Warehouse::where('wh_id', $warehouse_id);
-
-            if (!$wh) {
-                return back()->withErrors(['get_warehouse' => "ไม่มี"]);
-            }
-
-            return response()->json(['success' => true, 'warehouse' => $wh], 200);
-
-        } catch (\Exception $e) {
-            return back()->withErrors(['get_warehouse' => $e->getMessage()]);
         }
     }
 }
