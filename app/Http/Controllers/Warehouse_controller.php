e28cab5c6ec8022500c5ab5031c87720264c77fb
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-03-12 10:40:38 +0700

    refactor: delete not use function

--- app/Http/Controllers/Warehouse_controller.php
+++ app/Http/Controllers/Warehouse_controller.php
@@ -9,33 +9,10 @@ use Illuminate\Support\Facades\Session;
 
 class Warehouse_controller extends Controller
 {
-
-    public function index_warehouse()
-    {
-        try {
-
-            $warehouse_id = Session::get('user_warehouse');
-            if ($warehouse_id == null) {
-                $warehouse_id = Auth::user()->warehouses[0]->wh_id;
-            }
-
-
-            $wh = Warehouse::where('wh_id', $warehouse_id)->first();
-
-            if (!$wh) {
-                return back()->withErrors(['get_warehouse' => "ไม่มี"]);
-            }
-
-            return view('dashboards.v_all_wh')->with('wh', $wh);
-
-        } catch (\Exception $e) {
-            return back()->withErrors(['get_warehouse' => $e->getMessage()]);
-        }
-    }
-
     public function set_user_warehouse(Request $request)
     {
         try {
+
             $warehouse_id = $request->only('warehouse_id');
             Session::put('user_warehouse', $warehouse_id);
 
