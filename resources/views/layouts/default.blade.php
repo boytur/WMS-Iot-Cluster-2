bb690a1b68eee6587568f8d7fa7c12f9204f7dd4
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-04-03 18:04:59 +0700

    chore: delete sweet alert2 and jquery cdn

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -320,9 +320,6 @@
     </div>
 
 </body>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
 <script>
     /*
      * toggle_sidebar()
