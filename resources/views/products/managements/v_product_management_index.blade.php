e1fb754f2f794bffcf06d64759d6e3af2ea03ae5
Alisa <alisa.palung@gmail.com>
2024-04-06 11:03:09 +0700

    feat(v_product_management_index) : added title of table

--- resources/views/products/managements/v_product_management_index.blade.php
+++ resources/views/products/managements/v_product_management_index.blade.php
@@ -76,7 +76,7 @@
                 <div class="w-full bg-black/20 mt-2 rounded-md">
                     <div class="py-2 w-full bg-[#D9D9D9] rounded-t-md">
                             <b class="mx-2  mt-2 text-lg text-black uppercase   ">
-                                ตารางรายการสินค้า</b>
+                                ตารางรายการสินค้า </b>
                     </div>
                     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                         <table class="w-full text-sm text-left rtl:text-right ">
