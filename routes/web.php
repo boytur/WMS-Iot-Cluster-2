b637b7eedaaebc05c91d1f902f194e6ac853785f
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-04-06 20:43:25 +0700

    feat(route): added CreateLotInOrder route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Products\Inbounds\CreateLotInOrder;
 use App\Http\Controllers\Products\Inbounds\InboundIndex;
 use app\Http\Controllers\Products\Inbounds\GetLotInAnotherWh;
 use App\Http\Controllers\Products\Outbounds\OutboundIndex;
@@ -57,6 +58,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/product/inbounds/edit-inbound-order/{lot_in_id}', [InboundIndex::class, 'edit_inbound_order']);
 
     Route::post('/product/inbounds/search-lot-in', [InboundIndex::class, 'search_lot_in'])->name('search_lot_in');
+    Route::post('/product/inbounds/create-inbound-order', [CreateLotInOrder::class, 'create_inbound_order']);
 
     // route for outbound order
     Route::get('/product/outbounds', [OutboundIndex::class, 'outbound_index']);
@@ -93,5 +95,5 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/user-management', [UserManagementIndex::class, 'user_management_index'])->name('user_management_index');
     Route::get('/user-management/detail/{number}', [UserManagementIndex::class, 'user_management_detail'])->name('user_management_detail');
     Route::post('/user-management/search', [UserManagementIndex::class, 'search_user'])->name('search_user');
-    Route::get('/profile/{number}', [Profile::class,'get_user_profile']);
+    Route::get('/profile/{number}', [Profile::class, 'get_user_profile']);
 });
